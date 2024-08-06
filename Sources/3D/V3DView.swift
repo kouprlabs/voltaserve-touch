d669b38ae10a405e9d693772d17feafa593ad4d6
Anass Bouassaba <ebouassaba@gmail.com>
2024-08-06 04:51:20 +0200

    refactor: 3d

--- Sources/3D/V3DView.swift
+++ Sources/3D/V3DView.swift
@@ -2,38 +2,6 @@ import GLTFKit2
 import SceneKit
 import SwiftUI
 
-// Helper function to calculate the bounding box considering all child nodes and transformations
-extension SCNNode {
-    func boundingBoxRelativeToCurrentObject() -> (SCNVector3, SCNVector3) {
-        var minVec: SCNVector3 = SCNVector3Zero
-        var maxVec: SCNVector3 = SCNVector3Zero
-        var first = true
-
-        enumerateChildNodes { node, _ in
-            let (nodeMin, nodeMax) = node.boundingBox
-
-            // Apply node transformation to the bounding box
-            let transformedMin = node.convertPosition(nodeMin, to: self)
-            let transformedMax = node.convertPosition(nodeMax, to: self)
-
-            if first {
-                minVec = transformedMin
-                maxVec = transformedMax
-                first = false
-            } else {
-                minVec.x = Swift.min(minVec.x, transformedMin.x)
-                minVec.y = Swift.min(minVec.y, transformedMin.y)
-                minVec.z = Swift.min(minVec.z, transformedMin.z)
-                maxVec.x = Swift.max(maxVec.x, transformedMax.x)
-                maxVec.y = Swift.max(maxVec.y, transformedMax.y)
-                maxVec.z = Swift.max(maxVec.z, transformedMax.z)
-            }
-        }
-
-        return (minVec, maxVec)
-    }
-}
-
 struct V3DView: UIViewRepresentable {
     @ObservedObject var document: V3DDocument
 
@@ -155,7 +123,7 @@ struct V3DView: UIViewRepresentable {
         let sceneView = context.coordinator.sceneView
         sceneView.allowsCameraControl = true
         sceneView.autoenablesDefaultLighting = true
-        sceneView.backgroundColor = .white // Set the background color to white
+        sceneView.backgroundColor = .white
 
         context.coordinator.loadAsset()
 
@@ -163,4 +131,36 @@ struct V3DView: UIViewRepresentable {
     }
 
     func updateUIView(_: SCNView, context _: Context) {}
+}
+
+// Helper function to calculate the bounding box considering all child nodes and transformations
+extension SCNNode {
+    func boundingBoxRelativeToCurrentObject() -> (SCNVector3, SCNVector3) {
+        var minVec: SCNVector3 = SCNVector3Zero
+        var maxVec: SCNVector3 = SCNVector3Zero
+        var first = true
+
+        enumerateChildNodes { node, _ in
+            let (nodeMin, nodeMax) = node.boundingBox
+
+            // Apply node transformation to the bounding box
+            let transformedMin = node.convertPosition(nodeMin, to: self)
+            let transformedMax = node.convertPosition(nodeMax, to: self)
+
+            if first {
+                minVec = transformedMin
+                maxVec = transformedMax
+                first = false
+            } else {
+                minVec.x = Swift.min(minVec.x, transformedMin.x)
+                minVec.y = Swift.min(minVec.y, transformedMin.y)
+                minVec.z = Swift.min(minVec.z, transformedMin.z)
+                maxVec.x = Swift.max(maxVec.x, transformedMax.x)
+                maxVec.y = Swift.max(maxVec.y, transformedMax.y)
+                maxVec.z = Swift.max(maxVec.z, transformedMax.z)
+            }
+        }
+
+        return (minVec, maxVec)
+    }
 }
