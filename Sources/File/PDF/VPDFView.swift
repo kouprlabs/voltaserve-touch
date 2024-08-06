359179fdf5c348154358bb50af0d8870b3610b62
Anass Bouassaba <ebouassaba@gmail.com>
2024-08-06 19:30:41 +0200

    refactor: PDF view

--- Sources/File/PDF/VPDFView.swift
+++ Sources/File/PDF/VPDFView.swift
@@ -136,31 +136,6 @@ struct VPDFThumbnailListViewContainer: View {
     }
 }
 
-struct VPDFThumbnailView: View {
-    let index: Int
-    @ObservedObject var document: VPDFDocument
-    let pdfView: PDFView
-
-    var body: some View {
-        if let thumbnail = document.loadedThumbnails[index] {
-            Image(uiImage: thumbnail)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                // Background to show when no image
-                .background(Color.gray.opacity(0.2))
-                .frame(width: 100, height: 150, alignment: .center)
-                .onTapGesture {
-                    if let page = document.pdfDocument?.page(at: index) {
-                        pdfView.go(to: page)
-                    }
-                }
-        } else {
-            Color.gray.opacity(0.2)
-                .frame(width: 100, height: 150, alignment: .center)
-        }
-    }
-}
-
 struct VPDFThumbnailListView: View {
     @ObservedObject var document: VPDFDocument
     let pdfView: PDFView
@@ -207,6 +182,31 @@ struct VPDFThumbnailListView: View {
         // Move thumbnail loading to a background queue
         DispatchQueue.global(qos: .userInitiated).async {
             document.loadThumbnails(for: Array(indicesToLoad))
+        }
+    }
+}
+
+struct VPDFThumbnailView: View {
+    let index: Int
+    @ObservedObject var document: VPDFDocument
+    let pdfView: PDFView
+
+    var body: some View {
+        if let thumbnail = document.loadedThumbnails[index] {
+            Image(uiImage: thumbnail)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                // Background to show when no image
+                .background(Color.gray.opacity(0.2))
+                .frame(width: 100, height: 150, alignment: .center)
+                .onTapGesture {
+                    if let page = document.pdfDocument?.page(at: index) {
+                        pdfView.go(to: page)
+                    }
+                }
+        } else {
+            Color.gray.opacity(0.2)
+                .frame(width: 100, height: 150, alignment: .center)
         }
     }
 }
