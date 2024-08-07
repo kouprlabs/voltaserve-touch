866b55fdb78d570590cd9e722bc36b60bfe68e87
Anass Bouassaba <ebouassaba@gmail.com>
2024-08-07 05:16:38 +0200

    wip: update icon

--- Sources/File/Mosaic/MosaicView.swift
+++ Sources/File/Mosaic/MosaicView.swift
@@ -82,7 +82,7 @@ struct MosaicView: View {
                             }
                         }
                     } label: {
-                        Label("Zoom Levels", systemImage: "ellipsis")
+                        Label("Zoom Levels", systemImage: "ellipsis.circle")
                     }
                 }
             }
