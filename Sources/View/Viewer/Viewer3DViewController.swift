8c1fe88534557c9658ac81efedc1e87cfcb3dd9d
Anass Bouassaba <ebouassaba@gmail.com>
2024-08-11 08:20:19 +0200

    chore: cleanup

--- Sources/View/Viewer/Viewer3DViewController.swift
+++ Sources/View/Viewer/Viewer3DViewController.swift
@@ -4,6 +4,5 @@ class Viewer3DViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Setup your 3D viewer UI here
     }
 }
