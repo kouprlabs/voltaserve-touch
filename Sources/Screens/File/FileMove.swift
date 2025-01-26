2cbed4de0d8f3f67b0bbbba5b1cc58591d190125
Anass Bouassaba <ebouassaba@gmail.com>
2025-01-26 20:57:28 +0100

    fix: reset file selection after move and copy (#63)

--- Sources/Screens/File/FileMove.swift
+++ Sources/Screens/File/FileMove.swift
@@ -102,6 +102,7 @@ struct FileMove: View {
             errorSeverity = .full
             errorIsPresented = true
         } anyways: {
+            fileStore.selection = []
             isProcessing = false
         }
     }
