97b7fc7418bef5b5cb1e4cc23f6e292291f947ce
Anass Bouassaba <ebouassaba@gmail.com>
2025-06-08 20:04:52 +0200

    feat: thumbnail cache using Kingfisher

--- Package.swift
+++ Package.swift
@@ -15,12 +15,13 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/warrenm/GLTFKit2.git", from: "0.5.11")
+        .package(url: "https://github.com/warrenm/GLTFKit2.git", from: "0.5.11"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.3.2"),
     ],
     targets: [
         .target(
             name: "VoltaserveCore",
-            dependencies: ["GLTFKit2"],
+            dependencies: ["GLTFKit2", "Kingfisher"],
             path: "Sources"
         ),
         .testTarget(
